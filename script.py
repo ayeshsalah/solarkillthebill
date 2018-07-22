55cdf98b7e756700be0631114269019c018d2429
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2018-07-22 23:53:21 +0530

    moving repos

---
+++ script.py
@@ -0,0 +1,159 @@
+from flask import Flask, render_template, request, redirect, flash, send_from_directory
+import os
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        existing = ""
+        new_construction = ""
+        area = float(request.form.get('areaInput'))
+        load_kw = float(request.form.get('loadKW'))
+        load_hp = float(request.form.get('loadHP'))
+        units = float(request.form.get('unitsInput'))
+        output_per_day = float(request.form.get('outputPerDay'))
+        billing_type = request.form.get('billingOptionsRadios')
+        north_coordinates = request.form.get('northCoordinate')
+        east_coordinates = request.form.get('eastCoordinates')
+        building_height = request.form.get('heightInput')
+        floors = request.form.get('floorsInput')
+        construction_type = request.form.get('optionsRadios')
+        if construction_type == "existing":
+            existing = "checked"
+        if construction_type == "new construction":
+            new_construction = "checked"
+
+        contracted_load = (0.746 * load_hp) + load_kw
+
+        contracted_load = round(contracted_load * 2) / 2 # rounding to nearest 0.5
+
+        cca = 0
+        ccb = 0
+        if billing_type == "Commercial":
+            if 0 < contracted_load > 1:
+                cca = contracted_load * 70
+        else:
+            if 0 < contracted_load > 1:
+                cca = 1 * 50
+                ccb = (contracted_load - 1) * 60
+            else:
+                cca = contracted_load * 50
+
+        fuel_cess = round(units * 0.15, 2)
+        slab_rates = set_slab_rates(billing_type)
+        slabs = calculate_slabs(units, slab_rates, billing_type)
+        tax = round(((slabs[0] + slabs[1] + slabs[2] + slabs[3] + slabs[4] + slabs[5]) * 6)/100, 2)
+        recommended_value1 = round((units - 100)/120, 2)
+        # recommended_value2 = round(contracted_load * 0.75, 2)
+        recommended_value2 = round(area/70, 2)
+        # print(recommended_value1, recommended_value2, recommended_value3)
+        recommended_value = min([recommended_value1, recommended_value2, contracted_load]) 
+        net_bill = round(cca+ccb+slabs[0] + slabs[1] + slabs[2] + slabs[3] + slabs[4] + slabs[5]+fuel_cess+tax, 2)
+
+        expected_units = output_per_day * recommended_value * 30
+        units_after = units - expected_units 
+ 
+        slabs_after = calculate_slabs(units_after, slab_rates, billing_type)
+        fuel_cess_after = round(units_after * 0.15, 2)
+        tax_after = round(((slabs_after[0] + slabs_after[1] + slabs_after[2] + slabs_after[3] + slabs_after[4] + slabs_after[5]) * 6)/100, 2)
+        net_bill_after = round(cca+ccb+slabs_after[0] + slabs_after[1] + slabs_after[2] + slabs_after[3] + slabs_after[4] + slabs_after[5]+fuel_cess_after+tax_after, 2)
+        # print((0.746 * load_hp) + load_kw)
+        # print(contracted_load)
+        # print(slabs[0], slabs[1], slabs[2], slabs[3], slabs[4], slabs[5])
+        # print(tax)
+        # print(recommended_value)
+        # print(area, load_kw, load_hp, units, billing_type, output_per_day,
+        # north_coordinates, east_coordinates, building_height, floors, construction_type)
+
+        # flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return render_template('submitted.html', area=area, load_kw=load_kw, load_hp=load_hp, units=units,
+                               output=output_per_day, billing_type=billing_type, north=north_coordinates,
+                               east=east_coordinates, height=building_height, floors=floors, existing=existing,
+                               newconstruction=new_construction, cca=cca, ccb=ccb, slab1=slabs[0], slab2=slabs[1], slab3=slabs[2], slab4=slabs[3], slab5=slabs[4], slab6=slabs[5],
+                               fuelcess=fuel_cess, tax=tax, netbill=net_bill, contracted_load=contracted_load, killthebill = recommended_value, 
+                               slab1after=slabs_after[0], slab2after=slabs_after[1], slab3after=slabs_after[2], slab4after=slabs_after[3], slab5after=slabs_after[4], 
+                               slab6after=slabs_after[5], fuelcessafter=fuel_cess_after, taxafter=tax_after, netbillafter=net_bill_after, savings = round(net_bill-net_bill_after, 2),
+                               slab1rate=slab_rates[0], slab2rate=slab_rates[1], slab3rate=slab_rates[2], slab4rate=slab_rates[3], slab5rate=slab_rates[4], slab6rate=slab_rates[5],
+                               rv1=recommended_value1, rv2=recommended_value2)
+    else:
+        return render_template('home.html')
+
+def set_slab_rates(billing_type):
+    if billing_type=="Urban":
+        slab1_rate = 3.5
+        slab2_rate = 4.95
+        slab3_rate = 6.90
+        slab4_rate = 7.55
+        slab5_rate = 7.6
+        slab6_rate = 7.65
+    elif billing_type == "Rural":
+        slab1_rate = 3.15
+        slab2_rate = 4.40
+        slab3_rate = 5.95
+        slab4_rate = 5.95
+        slab5_rate = 5.95
+        slab6_rate = 5.95
+    elif billing_type=="Commercial":
+        slab1_rate = 7.75
+        slab2_rate = 8.75
+        slab3_rate = 0
+        slab4_rate = 0
+        slab5_rate = 0
+        slab6_rate = 0
+    return([slab1_rate, slab2_rate, slab3_rate, slab4_rate, slab5_rate, slab6_rate])
+
+def calculate_slabs(units, slab_rates, billing_type):
+    if billing_type == "Urban" or billing_type == "Rural":
+        slab1 = 0
+        slab2 = 0
+        slab3 = 0
+        slab4 = 0
+        slab5 = 0
+        slab6 = 0
+        if units > 30:
+            slab1 = 30 * slab_rates[0]
+            units = units - 30
+            if units > 70:
+                slab2 = 70 * slab_rates[1]
+                units = units - 70
+                if units > 100:
+                    slab3 = 100 * slab_rates[2]
+                    units = units - 100
+                    if units > 100:
+                        slab4 = 100 * slab_rates[3]
+                        units = units - 100
+                        if units > 100:
+                            slab5 = 100 * slab_rates[4]
+                            units = units - 100
+                            if units > 0:
+                                slab6 = units * slab_rates[5]
+                        else:
+                            slab5 = units * slab_rates[4]
+                    else:
+                        slab4 = units * slab_rates[3]
+                else:
+                    slab3 = units * slab_rates[2]
+            else:
+                slab2 = units * slab_rates[1]
+        else:
+            slab1 = units * slab_rates[0]
+        return([round(slab1, 2), round(slab2, 2), round(slab3, 2), round(slab4, 2), round(slab5,2), round(slab6,2)])
+    else:
+        slab1 = 0
+        slab2 = 0
+        if units > 50:
+            slab1 = 50 * slab_rates[0]
+            units = units - 50
+            if units > 0:
+                slab2 = units * slab_rates[1]
+        else:
+            slab1 = units * slab_rates[0]
+        return([round(slab1, 2), round(slab2, 2), 0, 0, 0, 0])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
